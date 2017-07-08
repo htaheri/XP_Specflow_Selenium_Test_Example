570357a999f02260d8088875a748a1c0200efae9
htaheri <hossein"tahery.com>
2017-07-09 00:33:31 +0100

    Add project files.

---
+++ XP_BDD_1.Tests/SampleUIFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: SampleUiFeature
+	So I can test that the website is working
+	As a programmer
+	I want to be able to launch the website
+	And navigate to the home page
+
+@mytag
+Scenario: Example Scenario
+	Given I am on the home page
+	When I enter 'some text' into the 'title' input field  
+		And I click the 'submit' control
+	Then 'some text' should be displayed in the 'results' field
---
+++ XP_BDD_1.Tests/SampleUiFeatureSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace XP_BDD_1.Tests
+{
+    [Binding]
+    public class SampleUiFeatureSteps
+    {
+
+        private ChromeDriver driver;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("disable-infobars");
+
+            driver = new ChromeDriver(options);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            driver.Url = "http://localhost:53017/Home.html";
+            Assert.AreEqual("FAD XP Course", driver.Title);
+        }
+        
+        [When(@"I enter '(.*)' into the '(.*)' input field")]
+        public void WhenIEnterIntoTheInputField(string text, string fieldId)
+        {
+            ScenarioContext.Current["title"] = text;
+            driver.FindElement(By.Id(fieldId)).SendKeys(text);
+
+        }
+
+        [When(@"I click the '(.*)' control")]
+        public void WhenIClickTheControl(string controlId)
+        {
+            driver.FindElement(By.Id(controlId)).Click();
+        }
+        
+        [Then(@"'(.*)' should be displayed in the '(.*)' field")]
+        public void ThenShouldBeDisplayedInTheField(string expectedText, string fieldId)
+        {
+            Assert.AreEqual(expectedText, driver.FindElement(By.Id(fieldId)).Text);
+        }
+    }
+}
